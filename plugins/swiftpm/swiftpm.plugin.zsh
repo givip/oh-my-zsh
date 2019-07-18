c17e9ef28b7eddf61877f8e51cefe9ff3adbfe70
Pataridze Givi <pataridzegivi@gmail.com>
2019-07-18 18:37:01 +0400

    Update swiftpm.plugin.zsh
    'swift package fetch' deprecated in swift 5, use 'swift package resolve'

--- plugins/swiftpm/swiftpm.plugin.zsh
+++ plugins/swiftpm/swiftpm.plugin.zsh
@@ -1,7 +1,8 @@
 # Some aliases to make your life with the Swift Package Manager faster ✌️
 
 alias spi='swift package init'
-alias spf='swift package fetch'
+alias spf='swift package fetch' #deprecated in swift 5, use `swift package resolve`
+alias spr='swift package resolve'
 alias spu='swift package update'
 alias spx='swift package generate-xcodeproj'
 alias sps='swift package show-dependencies'
